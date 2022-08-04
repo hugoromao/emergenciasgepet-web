bc6f4878c6ba064a6e2acf0bd0a32db86db773b4
Hugo Lima Romão <hugo8romao@gmail.com>
2022-08-04 15:55:36 -0400

    FEAT: SEO test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 import { SnackbarProvider } from 'notistack'
+import { NextSeo } from 'next-seo'
 
 import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
@@ -17,6 +18,26 @@ function App({ Component, pageProps }: AppProps) {
         horizontal: 'center'
       }}
     >
+      <NextSeo
+        title="1º congresso roraimense de trauma e emergências médicas"
+        description="24, 25, 26 de novembro."
+        openGraph={{
+          url: 'https://congresso-roraimense-de-trauma-e-emergencias-medicas-web.vercel.app/',
+          title: '1º congresso roraimense de trauma e emergências médicas',
+          description: 'Open Graph Description',
+          images: [
+            {
+              url: 'https://congrrmed.herokuapp.com/uploads/Whats_App_Image_2022_07_20_at_15_15_05_b835f2d84e.jpeg?updated_at=2022-08-04T19:42:02.703Z',
+              width: 492,
+              height: 495,
+              alt: 'Imagem do evento',
+              type: 'image/jpeg'
+            }
+          ],
+          site_name: '1º congresso roraimense de trauma e emergências médicas'
+        }}
+      />
+
       <ThemeProvider theme={theme}>
         <Head>
           <title>1º CONGRESSO RORAIMENSE DE TRAUMA E EMERGÊNCIAS MÉDICAS</title>
